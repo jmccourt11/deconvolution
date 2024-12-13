de7f84d6d3e7870d691dbf20973d835386b9451d
jmccourt11 <144829294+jmccourt11@users.noreply.github.com>
2024-12-13 11:57:21 -0600

    Update git.sh

--- git.sh
+++ git.sh
@@ -6,8 +6,4 @@ read -sp "Enter commit message: " commit
 echo $commit
 git commit -m $commit 
 token=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
-<<<<<<< HEAD
 git push
-=======
-git push 
->>>>>>> e2c5a0a9de4800a1657e831e020710cd35a4dc12
